4484a6910844aeef44d2c08a4cca86775108be8d
Vadim <maslovvadim398@gmail.com>
2023-05-03 11:45:06 +0300

    before trying to align an images

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -4,18 +4,28 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import data.Hero
 
@@ -25,9 +35,19 @@ class HeroesScreen {
 @Composable
 fun HeroesScreen(hero : Hero) {
     Card(elevation = CardDefaults.cardElevation(2.dp),
-        modifier = Modifier.clip(RoundedCornerShape(16.dp))
-        ) {
-        Row {//row for text on the left and image on the right
+        modifier = Modifier
+            .clip(RoundedCornerShape(16.dp))
+            .fillMaxWidth()
+            .padding(8.dp)
+            .height(88.dp)
+    ) {
+        Row (
+            Modifier
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp),
+            verticalAlignment = Alignment.CenterVertically
+
+                ) {//row for text on the left and image on the right
         HeroInfo(hero.nameRes, hero.descriptionRes)
         Spacer(Modifier.size(16.dp))
         HeroIcon(hero.imageRes)
@@ -41,17 +61,25 @@ fun HeroIcon (@DrawableRes heroIcon : Int, modifier: Modifier = Modifier) {
         painter = painterResource(heroIcon),
         contentDescription = null,
         Modifier
-            .size(72.dp)
+            .size(72.dp, 72.dp)
             .clip(RoundedCornerShape(8.dp))
-            .padding(8.dp)
-
+            //.padding(8.dp)
+            .fillMaxSize()
+            .aspectRatio(1f)
     )
 }
 
 @Composable
 fun HeroInfo(@StringRes name : Int,@StringRes descr : Int, modifier: Modifier = Modifier) {
-    Column() {
-        Text(text = name.toString())
-        Text(text = descr.toString())
+    Column(
+
+    ) {
+        Text(stringResource(id = name),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Text(stringResource(id = descr),
+            style = MaterialTheme.typography.bodyLarge,
+            softWrap = true
+        )
     }
 }
